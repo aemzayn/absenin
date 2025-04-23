0ac5c67548e2baebfef37da19967127810c14c38
Ahmad Muslih Zain <ahmad.muslih22@gmail.com>
2025-04-23 10:39:38 +0300

    remove sequelize

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -9,7 +9,7 @@ const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
   },
 };
 
